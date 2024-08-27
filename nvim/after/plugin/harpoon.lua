162592f4107c4d3b6f6e6844119a2599cf641845
Leo Schleier <43878374+leoschleier@users.noreply.github.com>
2024-08-27 22:04:09 +0200

    Update harpoon config

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -1,11 +1,11 @@
 harpoon = require("harpoon")
 
 harpoon:setup()
-vim.keymap.set("n", "<leader>ha", function() harpoon:list():append() end)
+vim.keymap.set("n", "<leader>ha", function() harpoon:list():add() end)
 vim.keymap.set("n", "<leader>hh", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
 vim.keymap.set("n", "<leader>hj", function() harpoon:list():select(1) end)
 vim.keymap.set("n", "<leader>hk", function() harpoon:list():select(2) end)
 vim.keymap.set("n", "<leader>hl", function() harpoon:list():select(3) end)
-vim.keymap.set("n", "<leader>hö", function() harpoon:list():select(4) end)
+vim.keymap.set("n", "<leader>h;", function() harpoon:list():select(4) end)
 
