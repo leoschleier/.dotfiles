a96076a0cbf5ce3e002736a008466ae91ba243c8
Leo Schleier <43878374+leoschleier@users.noreply.github.com>
2024-12-28 18:18:37 +0100

    Add ghostty config

--- scripts/symlinks.sh
+++ scripts/symlinks.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-ln -s $HOME/.dotfiles/nvim $HOME/.config/nvim
-ln -s $HOME/.dotfiles/zsh/.zshrc $HOME/.zshrc
+mkdir -p $HOME/.config
+
+ln -sfn $HOME/.dotfiles/nvim $HOME/.config/nvim
+ln -sfn $HOME/.dotfiles/zsh/.zshrc $HOME/.zshrc
+ln -sfn $HOME/.dotfiles/ghostty $HOME/.config/ghostty
