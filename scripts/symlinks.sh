91fea938fc37b88c94cdf5893080c8f619a01499
Leo Schleier <43878374+leoschleier@users.noreply.github.com>
2025-01-31 20:12:26 +0100

    Add aerospace config

--- scripts/symlinks.sh
+++ scripts/symlinks.sh
@@ -6,3 +6,4 @@ ln -sfn ~/.dotfiles/nvim ~/.config/nvim
 ln -sfn ~/.dotfiles/zsh/.zshrc ~/.zshrc
 ln -sfn ~/.dotfiles/ghostty ~/.config/ghostty
 ln -sfn ~/.dotfiles/tmux/.tmux.conf ~/.tmux.conf
+ln -sfn ~/.dotfiles/aerospace ~/.config/aerospace
