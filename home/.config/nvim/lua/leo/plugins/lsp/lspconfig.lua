5da6f57ec4c37017c8702b8684019a00664f2f66
leoschleier <43878374+leoschleier@users.noreply.github.com>
2026-04-14 12:24:45 +0200

    Add telescope lsp keymaps

--- home/.config/nvim/lua/leo/plugins/lsp/lspconfig.lua
+++ home/.config/nvim/lua/leo/plugins/lsp/lspconfig.lua
@@ -1,82 +1,94 @@
 return {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-        "hrsh7th/cmp-nvim-lsp",
-        {
-            "folke/lazydev.nvim",
-            ft = "lua",
-            opts = {
-                library = {
-                    { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-                },
-            },
-        },
-    },
-    config = function()
-        local cmp_nvim_lsp = require("cmp_nvim_lsp")
+	"neovim/nvim-lspconfig",
+	dependencies = {
+		"hrsh7th/cmp-nvim-lsp",
+		{
+			"folke/lazydev.nvim",
+			ft = "lua",
+			opts = {
+				library = {
+					{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+				},
+			},
+		},
+	},
+	config = function()
+		local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
-        vim.api.nvim_create_autocmd("LspAttach", {
-            group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-            callback = function(ev)
-                local opts = { buffer = ev.buf, silent = true }
+		vim.api.nvim_create_autocmd("LspAttach", {
+			group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+			callback = function(ev)
+				local opts = { buffer = ev.buf, silent = true }
 
-                opts.desc = "Show buffer diagnostics"
-                vim.keymap.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts)
+				opts.desc = "Show LSP definitions"
+				vim.keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)
 
-                opts.desc = "Show line diagnostics"
-                vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
+				opts.desc = "Show LSP references"
+				vim.keymap.set("n", "grr", "<cmd>Telescope lsp_references<CR>", opts)
 
-                opts.desc = "Restart LSP"
-                vim.keymap.set("n", "<leader>rs", "<cmd>LspRestart<CR>", opts)
-            end,
-        })
+				opts.desc = "Show LSP implementations"
+				vim.keymap.set("n", "gri", "<cmd>Telescope lsp_implementations<CR>", opts)
 
-        vim.api.nvim_create_autocmd("LspAttach", {
-            group = vim.api.nvim_create_augroup('lsp_attach_disable_ruff_hover', { clear = true }),
-            callback = function(args)
-                local client = vim.lsp.get_clients({ id = args.data.client_id })[1]
-                if client == nil then
-                    return
-                end
-                if client.name == 'ruff' then
-                    -- Disable hover in favor of Pyright
-                    client.server_capabilities.hoverProvider = false
-                end
-            end,
-            desc = 'LSP: Disable hover capability from Ruff',
-        })
+				opts.desc = "Show LSP type definitions"
+				vim.keymap.set("n", "grt", "<cmd>Telescope lsp_type_definitions<CR>", opts)
 
-        -- Enable autocompletion (assign to every lsp server config)
-        local capabilities = cmp_nvim_lsp.default_capabilities()
+				opts.desc = "Show line diagnostics"
+				vim.keymap.set("n", "grd", vim.diagnostic.open_float, opts)
 
-        local servers = {
-            dockerls = {},
-            jsonls = {},
-            lua_ls = {},
-            marksman = {},
-            pyright = {},
-            ruff = {},
-            rust_analyzer = {
-                settings = {
-                    ["rust-analyzer"] = {
-                        cargo = {
-                            allFeatures = true,
-                        },
-                        checkOnSave = true,
-                        check = {
-                            command = "clippy",
-                        },
-                    },
-                },
-            },
-            taplo = {},
-            yamlls = {},
-        }
+				opts.desc = "Show buffer diagnostics"
+				vim.keymap.set("n", "grD", "<cmd>Telescope diagnostics bufnr=0<CR>", opts)
 
-        for server, config in pairs(servers) do
-            config.capabilities = capabilities
-            vim.lsp.config(server, config)
-            vim.lsp.enable(server)
-        end
-    end,
+				opts.desc = "Restart LSP"
+				vim.keymap.set("n", "<leader>rs", "<cmd>LspRestart<CR>", opts)
+			end,
+		})
+
+		vim.api.nvim_create_autocmd("LspAttach", {
+			group = vim.api.nvim_create_augroup("lsp_attach_disable_ruff_hover", { clear = true }),
+			callback = function(args)
+				local client = vim.lsp.get_clients({ id = args.data.client_id })[1]
+				if client == nil then
+					return
+				end
+				if client.name == "ruff" then
+					-- Disable hover in favor of Pyright
+					client.server_capabilities.hoverProvider = false
+				end
+			end,
+			desc = "LSP: Disable hover capability from Ruff",
+		})
+
+		-- Enable autocompletion (assign to every lsp server config)
+		local capabilities = cmp_nvim_lsp.default_capabilities()
+
+		local servers = {
+			dockerls = {},
+			jsonls = {},
+			lua_ls = {},
+			marksman = {},
+			pyright = {},
+			ruff = {},
+			rust_analyzer = {
+				settings = {
+					["rust-analyzer"] = {
+						cargo = {
+							allFeatures = true,
+						},
+						checkOnSave = true,
+						check = {
+							command = "clippy",
+						},
+					},
+				},
+			},
+			taplo = {},
+			yamlls = {},
+		}
+
+		for server, config in pairs(servers) do
+			config.capabilities = capabilities
+			vim.lsp.config(server, config)
+			vim.lsp.enable(server)
+		end
+	end,
 }
--- home/.config/nvim/lua/leo/plugins/telescope.lua
+++ home/.config/nvim/lua/leo/plugins/telescope.lua
@@ -1,19 +1,23 @@
 return {
-    'nvim-telescope/telescope.nvim',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    config = function()
-        local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-        vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-        vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-        vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+	"nvim-telescope/telescope.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		-- optional but recommended
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+	},
+	config = function()
+		local builtin = require("telescope.builtin")
+		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+		vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+		vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
 
-        vim.keymap.set('n', '<leader>fF', function()
-            builtin.find_files({ hidden = true })
-        end, { desc = 'Telescope find files (hidden)' })
+		vim.keymap.set("n", "<leader>fF", function()
+			builtin.find_files({ hidden = true })
+		end, { desc = "Telescope find files (hidden)" })
 
-        vim.keymap.set('n', '<leader>fG', function()
-            builtin.live_grep({ additional_args = { '--hidden', '--glob=!.git/' } })
-        end, { desc = 'Telescope live grep (hidden)' })
-    end,
+		vim.keymap.set("n", "<leader>fG", function()
+			builtin.live_grep({ additional_args = { "--hidden", "--glob=!.git/" } })
+		end, { desc = "Telescope live grep (hidden)" })
+	end,
 }
